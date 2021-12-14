fe4c6ddc6c9e814a19f67b6dde1c792556f9860b
taem1n1 <youman7953@naver.com>
2021-12-15 08:35:45 +0900

    공통 인터페이스 분석

--- src/main/java/com/jpa/DataJpa/repository/MemberRepository.java
+++ src/main/java/com/jpa/DataJpa/repository/MemberRepository.java
@@ -3,7 +3,10 @@ package com.jpa.DataJpa.repository;
 import com.jpa.DataJpa.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    List<Member> findByUsername(String username); //구현하지 않아도 동작(별도의 Impl, Custom 없이)
 
 }
