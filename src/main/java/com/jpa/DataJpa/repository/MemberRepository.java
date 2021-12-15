4f53c896fd44cd2a1f890cf9e589acaec4a72b46
taem1n1 <youman7953@naver.com>
2021-12-15 17:52:57 +0900

    @Query로 리포지토리 메소드에 쿼리 정의

--- src/main/java/com/jpa/DataJpa/repository/MemberRepository.java
+++ src/main/java/com/jpa/DataJpa/repository/MemberRepository.java
@@ -2,10 +2,15 @@ package com.jpa.DataJpa.repository;
 
 import com.jpa.DataJpa.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age") // 오타가 나면 Application 로딩 시점에 에러가 잡힘 Named query와 비슷
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
