0bc63261b61a7ce3232693ad5edde304fc7f5e9e
taem1n1 <youman7953@naver.com>
2021-12-21 10:42:33 +0900

    Web 확장 - 도메인 클래스 컨버터

---
+++ src/main/java/com/jpa/DataJpa/controller/MemberController.java
@@ -0,0 +1,35 @@
+package com.jpa.DataJpa.controller;
+
+import com.jpa.DataJpa.model.Member;
+import com.jpa.DataJpa.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    // Domain Converter 사용전
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    // Domain Converter 사용후
+    @GetMapping("/members2/{id}")
+    public String findMember(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("user"));
+    }
+}
